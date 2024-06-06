d2bc2ce24eb7ce01c5d74e59fc98bcf7ef8dd15f
4l3os <leoso4268@gmail.com>
2024-06-05 19:26:01 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,19 @@
+#esto es un comentario:HOLA MUNDO
+
+print("Hola phyton")
+
+
+"""
+Este es un comentario
+en varias lineas ahre
+"""
+
+
+
+
+#Consultar el tipo de dato
+print(type("Hola phyton")) #tipo"str"
+print(type(5)) #tipo"int"
+print(type(1.5)) #tipo"float"
+print(type(True)) #tipo"bool"
+print(type(3+1j)) #tipo"complejo"

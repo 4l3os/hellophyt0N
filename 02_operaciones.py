77a7eec491b305a40de4b393e089b3617ca9edca
4l3os <leoso4268@gmail.com>
2024-06-06 20:13:37 -0500

    Update 02_operaciones.py

--- 02_operaciones.py
+++ 02_operaciones.py
@@ -1,3 +1,27 @@
+#Operadores matematicos
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(10%2)
+print(10//3)
+print(2**3)
+print(2**3 + 3 - 7 / 1 // 4)
+
+
+print("Hola" + "Phyton")
+print("Hola " *  int(2**3 + 3 - 7 / 1 // 4))
+
+#Operadores comparativos
+
+print(3>4)
+print(3<4)
+print(3>=4)
+print(3<=4)
+print(3==4)
+print(3!=4)
+
+
 
 
 

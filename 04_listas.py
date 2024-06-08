a85cb3f42b7fef8fbd02027b46be034b5cb855ba
4l3os <leoso4268@gmail.com>
2024-06-08 17:48:09 -0500

    Create 04_listas.py

---
+++ 04_listas.py
@@ -0,0 +1,40 @@
+#Listas
+
+my_list = list()
+my_other_list=[]
+
+
+print(len(my_list))
+
+my_list = [35, 24,62,52,30,30,17]
+
+print(my_list)
+print(len(my_list))
+
+
+my_other_list=[35,1.77,"Leo","Oscanoa"]
+
+print(type(my_other_list))
+print(type(my_list))
+
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-3])
+print(my_other_list[-4])
+print(my_other_list.count("Leo"))
+print(my_list.count(30))
+
+age, height, name, surname=my_other_list 
+print(name)
+
+name, height, age, surname=my_other_list[2] , my_other_list[1] ,my_other_list[0] ,my_other_list[3] 
+print(age)
+
+print(my_list + my_other_list)
+
+
+
+
+
+

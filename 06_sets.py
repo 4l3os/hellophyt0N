55c7407a964c3de242138a93f9aafa84e1daac2c
4l3os <leoso4268@gmail.com>
2024-06-10 18:10:53 -0500

    Update 06_sets.py

---
+++ 06_sets.py
@@ -0,0 +1,11 @@
+#sets
+
+my_set =set()
+print(type(my_set))
+
+my_other_set={}
+print(type(my_other_set)) #Inicialmente es un dict
+
+my_other_set={"Leo","Oscanoa", 35}
+print(type(my_other_set))
+

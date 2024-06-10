48a5fa3f8996c287f7a20e96ab2247006ef8fd62
4l3os <leoso4268@gmail.com>
2024-06-10 17:59:54 -0500

    Update 05_tuples.py

--- 05_tuples.py
+++ 05_tuples.py
@@ -22,6 +22,14 @@ print(my_tuple + my_other_tuple)
 my_sum_tuple=my_tuple+my_other_tuple
 print(my_sum_tuple)
 
+print(my_sum_tuple[3:6])
 
+my_tuple=list(my_tuple)
+print(type(my_tuple))
 
+my_tuple[4]="OSCASA"
+my_tuple.insert(1, "AZUL")
+my_tuple=tuple(my_tuple)
+print(my_tuple)
+print(type(my_tuple))
 

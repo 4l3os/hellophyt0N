979c1730a21ed4cd8623be474895e490513775a5
4l3os <leoso4268@gmail.com>
2024-06-10 18:33:51 -0500

    Update 06_sets.py

--- 06_sets.py
+++ 06_sets.py
@@ -18,3 +18,26 @@ print(my_other_set)#un set no es una estructura ordenada y no admite repetidos
 my_other_set.add("Oscasac")
 
 print(my_other_set)
+
+print("Leo" in my_other_set)
+print("Leoso" in my_other_set)
+
+my_other_set.remove("Leo")
+print(my_other_set)
+
+my_other_set.clear()
+print(my_other_set)
+print(len(my_other_set))
+
+#del my_other_set no se puede
+
+my_set= {"Leo","Oscanoa",21}
+my_list=list(my_set)
+print(my_list)
+
+print(my_list[0])
+
+
+
+
+

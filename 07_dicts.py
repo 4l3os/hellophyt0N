1247d7f90b218bac5511d3f7997aceee7c69b5d2
4l3os <leoso4268@gmail.com>
2024-06-13 16:55:09 -0500

    Create 07_dicts.py

---
+++ 07_dicts.py
@@ -0,0 +1,36 @@
+#Diccionarios
+
+
+my_dict= dict()
+my_other_dict={}
+
+print(type(my_dict))
+print(type(my_other_dict))
+
+my_other_dict={"Nombre":"Leo", "Apellido":"Oscanoa" , "Edad":22 , 1:"Python"}
+
+my_dict={"Nombre":"Leo", 
+        "Apellido":"Oscanoa", 
+        "Edad":22, 
+        "Lenguajes":{"Python","Swift","Kotlin"},
+        1:1.77
+        }
+
+print(my_other_dict)
+print(my_dict)
+
+
+print(len(my_other_dict))
+print(len(my_dict))
+
+print(my_dict["Nombre"])
+my_dict["Nombre"]="Pedro"
+
+print(my_dict["Nombre"])
+print(my_dict[1])
+
+
+
+
+
+

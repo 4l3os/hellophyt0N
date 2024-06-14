8e1db3e9ebaab619d9e74de5851391488658977d
4l3os <leoso4268@gmail.com>
2024-06-14 17:16:21 -0500

    Update 09_loops.py

--- 09_loops.py
+++ 09_loops.py
@@ -24,8 +24,23 @@ while my_condition < 20:
 print("LA EJECUCION CONTINUA")
 
 
+#for
+my_list=[35,24,62,52,30,30,17]
 
+for element in my_list:
+    print(element)
 
+my_tuple=(35,1.77,"Leo","Oscanoa","osac")
+for element in my_tuple:
+    print(element)
+
+my_set={"Leo","Oscanoa", 22}
+for element in my_set:
+    print(element)
+
+my_dict={"Nombre":"Leo", "Apellido":"Oscanoa" , "Edad":22 , 1:"Python"}
+for element in my_dict:
+    print(element)
 
 
 

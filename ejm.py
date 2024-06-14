05dd4b1b248a35b456f7560e056ef3081e83f07e
4l3os <leoso4268@gmail.com>
2024-06-14 16:30:49 -0500

    Create ejm.py

---
+++ ejm.py
@@ -0,0 +1,13 @@
+edad = 17
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+
+elif edad >= 13:
+    print("Eres adolescente.")    
+
+elif edad >= 15:
+    print("Eres 15 o por ahi")
+
+else:
+    print("Eres un niño.")

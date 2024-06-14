6a19aae4188944621113d18118a75bc6ea24df51
4l3os <leoso4268@gmail.com>
2024-06-14 17:08:09 -0500

    Update 09_loops.py

--- 09_loops.py
+++ 09_loops.py
@@ -8,21 +8,23 @@ my_condition=0
 while my_condition <10:
     print(my_condition)
     my_condition += 2
-    
 else: #es opcional
     print("Mi condiciones mayor o igual a 10")
-
 print("LA EJECUCION CONTINUA")
 
 
 
 
 while my_condition < 20:
-    my_condition+=2
+    my_condition += 1
     if my_condition==15:
-        print("Mi condicion es 15")
+        print("Se detiene la ejecucion")
+        break
+    print(my_condition)
+print("LA EJECUCION CONTINUA")
 
-    print("Mi condicion es menor que 20")
+
+
 
 
 

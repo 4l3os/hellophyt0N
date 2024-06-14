fae6ef88d6c4703d2ec5242834551ab5d437e21d
4l3os <leoso4268@gmail.com>
2024-06-14 18:25:56 -0500

    Update 10_funciones.py

--- 10_funciones.py
+++ 10_funciones.py
@@ -5,13 +5,13 @@ def my_function ():
 
 my_function ()
 
-def sum_two_values(first_number, second_number):
-    print(first_number + second_number)
+def sum_two_values(first_value, second_value):
+    print(first_value + second_value)
 
 sum_two_values(5,2)
 sum_two_values(4561,451)
-    
-
+sum_two_values("7","5")    
+sum_two_values(1.5,1.4)
 
 
 

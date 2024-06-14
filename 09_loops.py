c6fbde0ea651ad30239e7a79ce2a36e78a0ad943
4l3os <leoso4268@gmail.com>
2024-06-14 16:53:00 -0500

    Create 09_loops.py

---
+++ 09_loops.py
@@ -0,0 +1,34 @@
+#LOOPS
+
+#while
+
+my_condition=0
+
+
+while my_condition <10:
+    print(my_condition)
+    my_condition += 2
+    
+else: #es opcional
+    print("Mi condiciones mayor o igual a 10")
+
+print("LA EJECUCION CONTINUA")
+
+
+
+
+while my_condition < 20:
+    my_condition+=2
+    if my_condition==15:
+        print("Mi condicion es 15")
+
+    print("Mi condicion es menor que 20")
+
+
+
+
+
+
+
+
+

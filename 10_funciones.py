f340ffdd91bb99bd5f090ee98f0272773314abf1
4l3os <leoso4268@gmail.com>
2024-06-14 18:17:21 -0500

    Create 10_funciones.py

---
+++ 10_funciones.py
@@ -0,0 +1,18 @@
+#funciones
+
+def my_function ():
+    print("Esto es una funcion")
+
+my_function ()
+
+def sum_two_values(first_number, second_number):
+    print(first_number + second_number)
+
+sum_two_values(5,2)
+sum_two_values(4561,451)
+    
+
+
+
+
+

2c5270f10549b67b0c73f6064253a7dbbb54e7d9
4l3os <leoso4268@gmail.com>
2024-06-14 18:46:31 -0500

    Update 10_funciones.py

--- 10_funciones.py
+++ 10_funciones.py
@@ -13,6 +13,27 @@ sum_two_values(4561,451)
 sum_two_values("7","5")    
 sum_two_values(1.5,1.4)
 
+def sum_two_values_with_return(first_value, second_value):
+    my_sum=first_value+second_value
+    return my_sum
+my_result=sum_two_values_with_return(10,5)
+print(my_result)
 
 
+def print_name(name,surname):
+    print(f"{name} {surname}")
+print_name("leo", "oscanoa")
+print_name(surname="oscanoa", name="leo")
+
+
+def print_name_with_default(name,surname,alias="sin alias"):
+    print(f"{name} {surname} {alias}")
+print_name_with_default("leo", "oscanoa", "aleos")
+print_name_with_default("leo", "oscanoa")
+
+
+def print_texts(*text):
+    print(text)
+
+print_texts("Hola" , "python", "leo")
 

347396596fa68cb609e4b6d467237b8e84f2ebf2
4l3os <leoso4268@gmail.com>
2024-06-17 17:34:09 -0500

    Update 11_clases.py

--- 11_clases.py
+++ 11_clases.py
@@ -8,16 +8,23 @@ print(MyEmptyPerson())
 
 class Person:
     def __init__(self, name, surname, alias="sin alias"):
-        self.full_name=f"{name} {surname} ({alias})"
+        self.full_name=f"{name} {surname} ({alias})" #Propiedad publica
+        self.__name=name #Propiedad privada
+
+    def get_name(self):
+        return self.__name
+
     def walk(self):
         print(f"{self.full_name} esta caminando")    
 
 my_person=Person("leo", "oscanoa")
 print(my_person.full_name)
+print(my_person.get_name())
 my_person.walk()
 
 my_other_person= Person("Leo", "Oscanoa", "Oso")
 print(my_other_person.full_name)
+
 my_other_person.walk()
 my_person.full_name= "Ale Ramirez (El loco de los perros)"
 print(my_person.full_name)
